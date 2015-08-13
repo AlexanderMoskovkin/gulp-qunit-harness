9c345e53cc3bc7b60fb54a48e1a404e525e1a40c
Alex Mos <alexander.moskovkin@devexpress.com>
2015-08-13 16:52:52 +0300

    Functionality accomplished

---
+++ Gulpfile.js
@@ -0,0 +1,23 @@
+var del    = require('del');
+var gulp   = require('gulp');
+var babel  = require('gulp-babel');
+var eslint = require('gulp-eslint');
+
+gulp.task('clean', function (cb) {
+    del('lib', cb);
+});
+
+gulp.task('build', ['clean'], function () {
+    return gulp
+        .src('src/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('lib'));
+});
+
+gulp.task('lint', function () {
+    return gulp
+        .src('src/**/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
---
+++ src/index.js
@@ -0,0 +1,74 @@
+import fs from 'fs';
+import through2 from 'through2';
+import Promise from 'promise';
+import QunitHarness from 'qunit-harness';
+
+
+var readFile = Promise.denodeify(fs.readFile);
+
+function hang () {
+    return new Promise(function () {
+        // NOTE: hang forever
+    });
+}
+
+async function fillResource (src, path) {
+    var content = await readFile(path, 'utf-8');
+
+    return { src, content };
+}
+
+
+export default function (options) {
+    options = {
+        basePath:          options.basePath || '',
+        scripts:           options.scripts || [],
+        css:               options.css || [],
+        configApp:         options.configApp || null,
+        saucelabsSettings: options.saucelabsSettings || null
+    };
+
+    var tests        = [];
+    var qunitHarness = new QunitHarness();
+
+    async function runTests () {
+        var scripts = await Promise.all(options.scripts.map(item => fillResource(item.src, item.path)));
+        var css     = await Promise.all(options.css.map(item => fillResource(item.src, item.path)));
+
+        qunitHarness
+            .fixtures(options.basePath)
+            .scripts(scripts)
+            .css(css)
+            .tests(tests);
+
+        if (typeof options.configApp === 'function')
+            qunitHarness.configApp(options.configApp);
+
+        qunitHarness.create();
+
+        if (options.saucelabsSettings) {
+            qunitHarness.saucelabs(options.saucelabsSettings);
+            return await qunitHarness.run();
+        }
+
+        await hang();
+    }
+
+    return through2.obj(function (file, enc, done) {
+        tests.push(file.path);
+        done();
+    }, async function (done) {
+        var error = null;
+
+        try {
+            await runTests();
+        }
+        catch (err) {
+            error = err;
+        }
+        finally {
+            qunitHarness.close();
+            done(error);
+        }
+    });
+};
